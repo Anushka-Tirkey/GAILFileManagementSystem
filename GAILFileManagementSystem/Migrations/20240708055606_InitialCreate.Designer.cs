0263e6ffe646d637ce890345db2634bfd0c036c9
Anushka-Tirkey <anushka16apr02@gmail.com>
2024-07-08 11:28:05 +0530

    Edited some of the migrations

--- GAILFileManagementSystem/Migrations/20240708055606_InitialCreate.Designer.cs
+++ GAILFileManagementSystem/Migrations/20240708055606_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using GAILFileManagementSystem.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GAILFileManagementSystem.Migrations
 {
     [DbContext(typeof(VendorDBContext))]
-    partial class VendorDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240708055606_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
