90c74f1077accb4c50d624bf98fe35687fcd1496
Anushka-Tirkey <anushka16apr02@gmail.com>
2024-07-08 11:15:57 +0530

    Created and Pushed Migration file: GAILFMSVendorCreateDB

--- GAILFileManagementSystem/Migrations/20240704072200_GAILFMSVendorCreateDB.cs
+++ GAILFileManagementSystem/Migrations/20240704072200_GAILFMSVendorCreateDB.cs
@@ -11,9 +11,10 @@ namespace GAILFileManagementSystem.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Vendors",
+                name: "Vendors",    //name of the table = Vendors
                 columns: table => new
                 {
+                    //defining all the contracts
                     VendorId = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     vendorname = table.Column<string>(type: "varchar(100)", nullable: false),
@@ -29,7 +30,7 @@ namespace GAILFileManagementSystem.Migrations
         }
 
         /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
+        protected override void Down(MigrationBuilder migrationBuilder) //when you want to drop your table
         {
             migrationBuilder.DropTable(
                 name: "Vendors");
