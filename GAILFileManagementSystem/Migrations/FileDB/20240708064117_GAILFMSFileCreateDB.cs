7f21a293357f2cb68eb3de62b8d535ece07860b4
Shwejit Shree <shwejits@gmail.com>
2024-07-12 14:25:08 +0530

    changed in fileDBContext

--- GAILFileManagementSystem/Migrations/FileDB/20240708064117_GAILFMSFileCreateDB.cs
+++ GAILFileManagementSystem/Migrations/FileDB/20240708064117_GAILFMSFileCreateDB.cs
@@ -2,15 +2,15 @@
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
+namespace FILESMGMT.Migrations
 
-namespace FILESMGMT.Migrations.FileDB
 {
     public partial class GAILFMSFileCreateDB : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Files",
+                name: "File",
                 columns: table => new
                 {
                     FileId = table.Column<int>(type: "int", nullable: false)
@@ -20,36 +20,20 @@ namespace FILESMGMT.Migrations.FileDB
                     Open_Date = table.Column<DateTime>(type: "datetime2", nullable: false),
                     Closed_Date = table.Column<DateTime>(type: "datetime2", nullable: false),
                     Contract_No = table.Column<int>(type: "int", nullable: false),
-                    Vendor_name = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
-                    Vendor_address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Vendor_name = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    Vendor_address = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Status = table.Column<string>(type: "varchar(100)", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Files", x => x.FileId);
+                    table.PrimaryKey("PK_File", x => x.FileId);
                 });
-
-            migrationBuilder.AddColumn<string>(
-                name: "FileName",
-                table: "Files",
-                type: "nvarchar(max)",
-                nullable: true);
-
-            migrationBuilder.Sql(@"
-                UPDATE Files
-                SET FileName = LEFT([File Type], 3) + ' - ' + 
-                               CONVERT(varchar, Open_Date, 23) + ' - ' + 
-                               Vendor_name + ' - ' + 
-                               CAST(Contract_No AS varchar) + ' - ' + 
-                               CAST(FileId AS varchar)
-            ");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Files");
+                name: "File");
         }
     }
 }
-
--- GAILFileManagementSystem/Migrations/FileDB/FileDBContextModelSnapshot.cs
+++ GAILFileManagementSystem/Migrations/FileDB/FileDBContextModelSnapshot.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace FILESMGMT.Migrations.FileDB
+namespace GAILFileManagementSystem.Migrations
 {
     [DbContext(typeof(FileDBContext))]
     partial class FileDBContextModelSnapshot : ModelSnapshot
@@ -17,67 +17,54 @@ namespace FILESMGMT.Migrations.FileDB
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.18")
+                .HasAnnotation("ProductVersion", "6.0.32")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
 
             modelBuilder.Entity("FILESMGMT.Models.Files", b =>
-                {
-                    b.Property<int>("FileId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
+            {
+                b.Property<int>("FileId")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
 
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("FileId"), 1L, 1);
+                SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("FileId"), 1L, 1);
 
-                    b.Property<DateTime>("Closed_Date")
-                        .HasColumnType("datetime2");
+                b.Property<DateTime>("Closed_Date")
+                    .HasColumnType("datetime2");
 
-                    b.Property<int>("Contract_No")
-                        .HasColumnType("int");
+                b.Property<int>("Contract_No")
+                    .HasColumnType("int");
 
-                    b.Property<string>("Description")
-                        .IsRequired()
-                        .HasColumnType("varchar(100)")
-                        .HasColumnName("Description");
+                b.Property<string>("Description")
+                    .IsRequired()
+                    .HasColumnType("varchar(100)")
+                    .HasColumnName("Description");
 
-                    b.Property<string>("FileName")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                b.Property<string>("File_type")
+                    .IsRequired()
+                    .HasColumnType("varchar(20)")
+                    .HasColumnName("File Type");
 
-                    b.Property<string>("FilePath")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                b.Property<DateTime>("Open_Date")
+                    .HasColumnType("datetime2");
 
-                    b.Property<long>("FileSize")
-                        .HasColumnType("bigint");
+                b.Property<string>("Status")
+                    .IsRequired()
+                    .HasColumnType("varchar(100)")
+                    .HasColumnName("Status");
 
-                    b.Property<string>("File_type")
-                        .IsRequired()
-                        .HasColumnType("varchar(20)")
-                        .HasColumnName("File Type");
+                b.Property<string>("Vendor_address")
+                    .HasColumnType("nvarchar(max)");
 
-                    b.Property<DateTime>("Open_Date")
-                        .HasColumnType("datetime2");
+                b.Property<string>("Vendor_name")
+                    .HasMaxLength(15)
+                    .HasColumnType("nvarchar(15)");
 
-                    b.Property<string>("Status")
-                        .IsRequired()
-                        .HasColumnType("varchar(100)")
-                        .HasColumnName("Status");
+                b.HasKey("FileId");
 
-                    b.Property<string>("Vendor_address")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("Vendor_name")
-                        .IsRequired()
-                        .HasMaxLength(15)
-                        .HasColumnType("nvarchar(15)");
-
-                    b.HasKey("FileId");
-
-                    b.ToTable("File", (string)null);
-                });
+                b.ToTable("File");
+            });
 #pragma warning restore 612, 618
         }
     }
